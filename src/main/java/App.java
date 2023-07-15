2c8952bd73bef7f7fa64a1fee9d548cedb0b73d0
Stepka1709 <stepanmichyrin@gmail.com>
2023-07-15 20:27:18 +0300

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanTwo = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat catTwo = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == beanTwo);
+        System.out.println(cat == catTwo);
     }
 }
